1f20020afcecab7611ee2bcf6a947024e1f4106c
Max Schulte <maximilliangschulte@pm.me>
2022-07-05 15:56:06 -0500

    scrollable pepe

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,43 @@
 <script>
-	export let name;
+	import MediaBox from "./MediaBox.svelte";
+	import { writable } from "svelte/store";
+
+	const pepe = "https://image.cnbcfm.com/api/v1/image/103978904-The_meme_formerly_known_as_Kuk_1.png?v=1475149183&w=740&h=416";
+
+	let y = 0;
+	const columns = 4;
+	const pepes = 1000;
+	const rows = Math.ceil(pepes / columns);
+
+	let minId = 0;
+	let maxId = 0;
+
+	$: {
+		// This doesn't have to be the window height, it could be a component.
+		const totalScroll = document.documentElement.scrollHeight - window.innerHeight;
+		if (Number.isFinite(y / totalScroll)) {
+			let minRow = Math.floor(y / document.documentElement.scrollHeight * rows);
+			let maxRow = Math.ceil((y + window.innerHeight) / document.documentElement.scrollHeight * rows);
+			minId = (minRow + 1) * columns
+			maxId = (maxRow - 1) * columns
+		}
+	}
 </script>
 
+<svelte:window bind:scrollY={y}/>
+
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<div id="grid">
+	{#each Array.from(Array(pepes).keys()) as i}
+		<MediaBox src={pepe} render={(minId <= i) && (i < maxId)}/>
+	{/each}
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+#grid {
+	display: grid;
+	grid-template-columns: repeat(4, 1fr);
+	padding: 0px 30%;
+	background-color: red;
+}
 </style>
---
+++ src/MediaBox.svelte
@@ -0,0 +1,25 @@
+<script>
+	export let render = false;
+	export let src = "";
+</script>
+
+<div id="container">
+	{#if render}
+		<img src={src}/>
+	{/if}
+</div>
+
+<style>
+#container{
+	display: flex;
+	justify-content: center;
+	width: 100px;
+	height: 100px;
+	background-color: blue;
+}
+img {
+	object-fit: scale-down;
+	max-width: 100%;
+	max-height: 100%;
+}
+</style>
